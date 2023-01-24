70d49480ddda5c77793f1082e0ab3690a6c7ac02
Élio Muculo <elodio.muculo@uem.ac.mz>
2023-01-24 10:16:49 +0200

    add animation

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -16,8 +22,14 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,24 +57,184 @@ import androidx.compose.ui.zIndex
 import com.example.app.afirmation.data.Datasource
 import com.example.app.afirmation.model.Afirmation
 import com.example.app.courses.Course
+import com.example.app.dog.Dog
 import com.example.app.ui.theme.AppTheme
 import kotlin.text.Typography
+import com.example.app.dog.dogs
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Surface (
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            )
-            {
-                TopicApp()
-            }
+                AppTheme {
+                    WoofApp()
+                }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofApp() {
+   Scaffold(
+       topBar = {
+           WoofAppBar()
+       },
+       content = { padding ->
+           LazyColumn(modifier = Modifier
+               .padding(padding)
+               .background(MaterialTheme.colorScheme.background)) {
+               items(dogs) {
+                   DogItem(dog = it)
+               }
+           }
+       }
+   )
+}
+
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+
+@Composable
+fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    val Green25 = Color(0xFFFF0E464D)
+    val color by animateColorAsState(
+        targetValue = if (expanded) Green25 else MaterialTheme.colorScheme.surface,
+    )
+
+
+    ElevatedCard(modifier = modifier.padding(8.dp)) {
+        Column (
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioHighBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+                .background(color = color)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(modifier = Modifier.weight(1f))
+                DogItemButton(expanded = expanded, onClick = {
+                    expanded = !expanded
+                })
+            }
+            if (expanded) DogHobby(dogHobby = dog.hobbies)
+        }
+    }
+}
+
+
+@Composable
+fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = dogIcon),
+        contentDescription = null,
+        modifier = Modifier
+            .size(64.dp)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(50)),
+        contentScale = ContentScale.Crop
+    )
+}
+
+
+@Composable
+fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = stringResource(id = dogName),
+            modifier = Modifier.padding(top = 8.dp),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            text = stringResource(id = R.string.years_old, dogAge),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+
+@Composable
+fun WoofAppBar(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .background(MaterialTheme.colorScheme.primary), verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = painterResource(id = R.drawable.ic_woof_logo), contentDescription = null, modifier = Modifier
+            .size(64.dp)
+            .padding(8.dp))
+        Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.displayLarge)
+    }
+}
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colorScheme.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description)
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun DogIconPreview() {
+    AppTheme {
+        WoofAppBar()
+    }
+}
+
+@Preview
+@Composable
+fun DarkDogIconPreview() {
+    AppTheme (darkTheme = true) {
+        WoofApp()
+    }
+}
+
+
+@Preview
+@Composable
+fun BirthdayPreview() {
+    AppTheme() {
+        BirthdayGreetingWithImage("Happy Birthday Elio", "Elliot")
+    }
+}
 
 @Composable
 fun TopicApp() {
@@ -436,11 +608,3 @@ fun BirthdayGreatingWithText(message: String, from: String) {
         )
     }
 }
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppTheme {
-        TopicCard(topic = Course(R.string.architecture, 58, R.drawable.architecture))
-    }
-}
-
